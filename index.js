acaa5e6a00272a39e1d443c5399ab049c4c4a64d
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-07 12:43:56 -0400

    commit tree 9488bcfc68a3179ddaedcfc389d57e1d1d8d461d

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400) {
+    return 'This one is on me!'
+  } else if (someNumber <= 2000){
+    return 'That will be twenty bucks.';
+  } else if (someNumber <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (someNumber > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(gen){
   // Write your code here!
+  
+  switch (gen) {
+    case 'generous': return 'Thank you so much.';
+    case 'not as generous': return 'Thank you.'; 
+    case 'thanks for everything': return 'Bye.';
+  }
 }
